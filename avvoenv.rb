11d51dbe04303fd311cb8e933012366c119f2c9b
Mat Sadler <mat@sourcetagsandcodes.com>
2017-10-09 15:43:27 -0700

    add high_sierra bottle for avvoenv

--- avvoenv.rb
+++ avvoenv.rb
@@ -5,6 +5,11 @@ class Avvoenv < Formula
   sha256 "ad32356fd483babae3f5903ff0a43fe666d2f16922ea3b8ad4aa1305c7d74bcb"
   head "https://github.com/avvo/avvoenv.git"
 
+  bottle do
+    root_url "https://github.com/avvo/avvoenv/releases/download/v0.2.0"
+    sha256 "a3098466fd8524c2d130eae50ce55735b6a04b3355573592cc0f54da326d8ab9" => :high_sierra
+  end
+
   depends_on "rust" => :build
 
   def install
