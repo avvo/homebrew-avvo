a6beda08b4084d17be3650c783c1588df6a444e4
Mat Sadler <mat@sourcetagsandcodes.com>
2017-10-13 12:53:15 -0700

    update avvoenv 0.2.1

--- avvoenv.rb
+++ avvoenv.rb
@@ -1,14 +1,9 @@
 class Avvoenv < Formula
   desc "Fetches the environment variables for Avvo services"
   homepage "https://github.com/avvo/avvoenv"
-  url "https://github.com/avvo/avvoenv/archive/v0.2.0.tar.gz"
-  sha256 "ad32356fd483babae3f5903ff0a43fe666d2f16922ea3b8ad4aa1305c7d74bcb"
+  url "https://github.com/avvo/avvoenv/archive/v0.2.1.tar.gz"
+  sha256 "f1c31bead12e55d89ce7754c0dddbc2d7f83e52a5b167e079f6476ee6276d48c"
   head "https://github.com/avvo/avvoenv.git"
-
-  bottle do
-    root_url "https://github.com/avvo/avvoenv/releases/download/v0.2.0"
-    sha256 "a3098466fd8524c2d130eae50ce55735b6a04b3355573592cc0f54da326d8ab9" => :high_sierra
-  end
 
   depends_on "rust" => :build
 
