83b06add1e55139b7bac03696bf7f869f14d8409
Mat Sadler <mat@sourcetagsandcodes.com>
2017-10-16 13:00:59 -0700

    bottle rsh 0.1.0

--- rsh.rb
+++ rsh.rb
@@ -5,6 +5,13 @@ class Rsh < Formula
   sha256 "c71dbbc95d494633a315de14bff9b50e43fe0d2d7fddfee46dbca6e8ed6c3ebd"
   head "https://github.com/avvo/rsh.git"
 
+  bottle do
+    root_url "https://github.com/avvo/rsh/releases/download/v0.1.0"
+    sha256 "0921a57221275dee993fcaeb9743748eb434799b057b14688397d84a7e6dc826" => :el_capitan
+    sha256 "325fbd6cd833a77afa77292613b6a7fcd4b1a7b771991519cae975ad5060a2b7" => :sierra
+    sha256 "9d923bc98681f963c8d5677298bf1db17ecaacdf26f919476097d9170d78bafc" => :high_sierra
+  end
+
   depends_on "rust" => :build
 
   def install
